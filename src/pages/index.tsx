506ed6f2cc643cb51a84ddb2171bd03a81ecc1d9
Cole Smith <cole.f.smith1@gmail.com>
2022-12-03 18:50:18 -0500

    test: range input (17)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -82,7 +82,7 @@ export default function HomePage() {
             <div className='mt-10'>
               {innerDimensions.height} | {innerDimensions.width}
             </div>
-            <div className='fixed top-10 left-0 right-0 bottom-0'>
+            <div className='top-30 fixed left-0 right-0 bottom-0'>
               <input
                 id='default-range'
                 type='range'
@@ -94,7 +94,6 @@ export default function HomePage() {
                 // }}
               />
             </div>
-            <div className='absolute top-20 left-0 right-0 bottom-0'>Hi!</div>
           </div>
 
           {/* <footer className='absolute bottom-2 text-gray-700'>
