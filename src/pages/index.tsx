d0522010381a13f3971c59bfb181fe653f4bfd46
Cole Smith <cole.f.smith1@gmail.com>
2022-12-03 19:00:41 -0500

    test: range input (24)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -84,7 +84,7 @@ export default function HomePage() {
             </div>
             <div
               className='fixed left-0 right-0 bottom-0'
-              style={{ top: '270px' }}
+              style={{ top: '280px' }}
             >
               <input
                 id='default-range'
